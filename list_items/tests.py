703af29b8f3d6a81c7a52f192124a3c358043a71
PGA <paulgadam@gmail.com>
2022-09-03 01:23:07 -0600

    added a test for the homepage

--- list_items/tests.py
+++ list_items/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+import pytest
 
+def test_homepage_access():
+    url = reverse('list_items-list')
+    assert url == "/"
 # Create your tests here.
+
+
