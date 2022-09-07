427057b7100d8a54f202b9762cc5f056cb2ef1f0
PGA <paulgadam@gmail.com>
2022-09-06 20:56:31 -0600

    updated tests

--- list_items/tests.py
+++ list_items/tests.py
@@ -28,10 +28,10 @@ def test_search_grocery(new_groc):
     assert List_items.objects.filter(groc_item='Pytest').exists()
 
 def test_update_grocery(new_groc):
-    new_groc.groc_item = 'Pytest-Django'
+    new_groc.groc_item = 'Pytest'
     new_groc.save()
-    assert List_items.objects.filter(groc_item='Pytest-Django').exists()
+    assert List_items.objects.filter(groc_item='Pytest').exists()
 
 def test_delete_grocery(new_groc):
     new_groc.delete()
-    assert (List_items.objects.filter(groc_item='Pytest-Django').exists()) == False
+    assert (List_items.objects.filter(groc_item='Pytest').exists()) == False
